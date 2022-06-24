a8b97c25030994975145ca3ca8f3427c74cfd124
Nabil Amin <nabil.m.amin@gmail.com>
2022-06-24 12:40:15 -0400

    Update

--- script.js
+++ script.js
@@ -1,93 +1,270 @@
+const playerHand = document.querySelectorAll('.btn-key');
+const output = document.querySelector('.output');
+const playerScore = document.querySelector('.playerscore');
+const computerScore = document.querySelector('.computerscore');
+const playerIcon = document.querySelector('.player-icon');
+const computerIcon = document.querySelector('.computer-icon');
+const rockImgSrc = 'images/hand-fist-solid.svg';
+const paperImgSrc = 'images/hand-solid.svg';
+const scissorsImgSrc = 'images/hand-scissors-solid.svg';
 
+let i = 0;
+let playerRound = 0;
+let computerRound = 0;
+let winnerRound = '';
 
-
-function computerPlay() {
-    const gameInt = Math.floor(Math.random() * 3);
-    let gameHand = ""
-    if (gameInt === 0) {
-        gameHand = "rock"
-    } else if (gameInt === 1) {
-        gameHand = "paper"
-    } else {
-        gameHand = "scissors"
-    }
-
-    return gameHand
+function computerHand() {
+    const round = Math.floor(Math.random() * 3);
     
-}
-
-const computerSelection = computerPlay();
-
-// console.log(gameInt)
-console.log(computerPlay)
-console.log(computerSelection)
-
-
-// function choose() {
-//     let playerSelect = prompt("Rock, Paper, or Scissors?").toLowerCase();
-//     if (playerSelect.toLowerCase() !== "rock" || playerSelect !== "paper" || playerSelect !== "scissors") {
-//         playerSelect = prompt("Please only choose rock, paper, or scissors only!").toLowerCase();
-//         return playerSelect
-//     } else {
-//         return playerSelect
-//     }
-// }
-// const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-
-// console.log(playerSelection)
-
-
+    switch (round) {
+        case 0:
+            computerIcon.src = rockImgSrc
+            return 'rock'
+        case 1:
+            computerIcon.src = paperImgSrc
+            return 'paper'
+        case 2:
+            computerIcon.src = scissorsImgSrc
+            return 'scissors'
+    }
+};
 
 function playRound(playerSelection, computerSelection) {
-    // rock beats scissors, paper beats rock, scissors beats paper
-    // method 1: 
-    // if playerSelection === computerSelection then "Its a tie! You both chose {computerSelection}."
-    // going to have to account for all scenarios manually. 
-
-    // let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-
     if (playerSelection === computerSelection) {
-        console.log("It's a tie! You both chose ",computerSelection)
-        playerRound++;
-        computerRound++;
+        output.innerHTML = (`It's a tie! You both choose ${computerSelection}`)
+        winnerRound = 'tie'
     } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper"))  {
-        console.log("You won! ",playerSelection," beats",computerSelection)
+        output.innerHTML = (`You won! ${playerSelection} beats ${computerSelection}.`)
         playerRound++;
+        winnerRound = 'player'
     } else {
-        console.log("You Lost! ",computerSelection," beats",playerSelection)
+        output.innerHTML = (`You lost! ${computerSelection} beats ${playerSelection}.`)
         computerRound++;
+        winnerRound = 'computer'
     }
-    console.log(playerRound)
-    console.log(computerRound)
     score = [playerRound, computerRound]
     return score
 }
 
-// console.log(playRound(playerSelection,computerSelection))
+for (i; i < 3; i++) {
+    playerHand[i].addEventListener('click', function() {
 
 
+        // let score = [playerRound, computerRound]
 
-    let playerRound = 0;
-    let computerRound = 0;
-    let i = 0;
+        if (playerRound === 5) {
+            output.innerHTML = 'Congrats! You have won the game!'
+        } else if (computerRound === 5) {
+            output.innerHTML = 'Sorry! You have lost the game!'
+        }
+        else {
+            const computerSelection = computerHand();
+            let playerSelection = '';
+        
+            if (this.getAttribute('data-hand') === "rock") {
+                playerSelection = 'rock'
+                playerIcon.src = rockImgSrc
+            } else if (this.getAttribute('data-hand') === 'paper') {
+                playerSelection = 'paper'
+                playerIcon.src = paperImgSrc
+            } else {
+                playerSelection = 'scissors'
+                playerIcon.src = scissorsImgSrc
+            }
+
+            playRound(playerSelection, computerSelection)
+
+            console.log(winnerRound)
+            // switch (winnerRound) {
+            //     case 'tie':
+            //         playerIcon.classList.remove('win')
+            //         playerIcon.classList.remove('lose')
+            //         computerIcon.classList.remove('win')
+            //         computerIcon.classList.remove('lose')
+            //     case 'player':
+            //         playerIcon.classList.remove('lose')
+            //         playerIcon.classList.add('win')
+            //         computerIcon.classList.remove('win')
+            //         computerIcon.classList.add('lose')
+            //     case 'computer':
+            //         playerIcon.classList.remove('win')
+            //         playerIcon.classList.add('lose')
+            //         computerIcon.classList.remove('lose')
+            //         computerIcon.classList.add('win')
+            // }
+
+        }
+        
+        // if (score[0] === '5' && score[0] === score[1]) {
+        //     output.innerHTML = 'After 5 rounds, its a tie!'
+        //     score = [0,0]
+        //     return
+        // } else if (score[0] === '5' && score[0] > score[1]) {
+        //     output.innerHTML = 'After 5 rounds, YOU WON!'
+        //     score = [0,0]
+        //     return
+        // } else if (score[1] === '5' && score[1] > score[0]) {
+        //     output.innerHTML = 'After 5 rounds, the computer won.'
+        //     score[0,0]
+        //     return
+        // } else {
+            
+        // }
 
 
-    for (i; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, or Scissors").toLowerCase()
-        const computerSelection = computerPlay()
-        console.log(playRound(playerSelection,computerSelection))
-        console.log("Your score is", playerRound)
-        console.log("The computer's score is", computerRound)
+        playerScore.innerHTML = (`Player: ${playerRound}`);
+        computerScore.innerHTML = (`Computer: ${computerRound}`);
 
         
-    }
-
-    if (playerRound > computerRound) {
-            console.log("After 5 rounds, you're the winner!")
-        } else if (computerRound>playerRound) {
-            console.log("After 5 rounds, the computer won!")
-        } else {
-            console.log("After 5 rounds, its a draw!")
-        }
-
     
+
+})
+}
+
+
+// // Anything below this line is just work in progress, testing ideas, or notes.
+// // Computer chooses their hand. 
+// function computerPlay() {
+//     const gameInt = Math.floor(Math.random() * 3);
+//     let gameHand = ""
+//     if (gameInt === 0) {
+//         gameHand = "rock"
+//     } else if (gameInt === 1) {
+//         gameHand = "paper"
+//     } else {
+//         gameHand = "scissors"
+//     }
+//     return gameHand  
+// }
+
+// const computerSelection = computerPlay(); //assign function to a variable for computer hand.
+
+// // console.log(gameInt)
+// console.log(computerPlay)
+// console.log(computerSelection)
+
+
+// // function choose() {
+// //     let playerSelect = prompt("Rock, Paper, or Scissors?").toLowerCase();
+// //     if (playerSelect.toLowerCase() !== "rock" || playerSelect !== "paper" || playerSelect !== "scissors") {
+// //         playerSelect = prompt("Please only choose rock, paper, or scissors only!").toLowerCase();
+// //         return playerSelect
+// //     } else {
+// //         return playerSelect
+// //     }
+// // }
+// // const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+// // console.log(playerSelection)
+
+
+
+// function playRound(playerSelection, computerSelection) {
+//     // rock beats scissors, paper beats rock, scissors beats paper
+//     // method 1: 
+//     // if playerSelection === computerSelection then "Its a tie! You both chose {computerSelection}."
+//     // going to have to account for all scenarios manually. 
+
+//     // let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+
+//     if (playerSelection === computerSelection) {
+//         console.log("It's a tie! You both chose ",computerSelection)
+//         playerRound++;
+//         computerRound++;
+//     } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper"))  {
+//         console.log("You won! ",playerSelection," beats",computerSelection)
+//         playerRound++;
+//     } else {
+//         console.log("You Lost! ",computerSelection," beats",playerSelection)
+//         computerRound++;
+//     }
+//     console.log(playerRound)
+//     console.log(computerRound)
+//     score = [playerRound, computerRound]
+//     return score
+// }
+
+// // console.log(playRound(playerSelection,computerSelection))
+
+
+
+// let playerRound = 0;
+// let computerRound = 0;
+// let i = 0;
+
+// // const plyr_rock = document.querySelector('#btn-1');
+// // const plyr_paper = document.querySelector('#btn-2');
+// // const plyr_scissors = document.querySelector('#btn-3');
+
+// // clicking on these player buttons will do a number of things:
+// // 1. it will load up the player's hand icon on the center screen
+// // 2. it will return the value of the players chosen hand (rock, paper, scissors)
+// // 3. it will initiate the game and the computers choice
+
+// // should we do queryselectorall and iterate over the three choices with the appropriate IDs (#btn-1 > scissors, #btn-2 > rock, #btn-3 > paper)
+
+// const playerHand = document.querySelectorAll('.btn-key');
+
+// // function determinePlayerHand() {
+// //     if (this.getAttribute('data-hand') === "rock") {
+// //         console.log("rock")
+// //         return "rock"
+// //     } else if (this.getAttribute('data-hand') === "paper") {
+// //         console.log("paper")
+// //         return "paper"
+// //     } else if (this.getAttribute('data-hand') === "scissors") {
+// //         console.log("scissors")
+// //         return "scissors"
+// //     }
+// // } 
+
+// function highlightBox() {
+//     this.classList.add('playing')
+// }
+
+// for (i; i < 3; i++) {
+//     playerHand[i].addEventListener('click', function() {
+//         // this.classList.add('playing') // update the button's style (css class)
+
+//         let playerSelection = ""
+
+//         if (this.getAttribute('data-hand') === "rock") {
+//             console.log("rock")
+//             playerSelection = "rock";
+//         } else if (this.getAttribute('data-hand') === "paper") {
+//             console.log("paper")
+//             playerSelection = "paper";
+//         } else if (this.getAttribute('data-hand') === "scissors") {
+//             console.log("scissors")
+//             playerSelection = "scissors";
+//         }
+
+//         const computerSelection = computerPlay();
+
+//         console.log(playRound(playerSelection, computerSelection));
+
+
+
+//     }
+//     );
+// }
+
+
+
+//     // for (i; i < 5; i++) {
+//     //     let playerSelection = prompt("Rock, Paper, or Scissors").toLowerCase()
+//     //     const computerSelection = computerPlay()
+//     //     console.log(playRound(playerSelection,computerSelection))
+//     //     console.log("Your score is", playerRound)
+//     //     console.log("The computer's score is", computerRound)
+
+        
+//     // }
+
+//     // if (playerRound > computerRound) {
+//     //         console.log("After 5 rounds, you're the winner!")
+//     //     } else if (computerRound>playerRound) {
+//     //         console.log("After 5 rounds, the computer won!")
+//     //     } else {
+//     //         console.log("After 5 rounds, its a draw!")
+//     //     }
